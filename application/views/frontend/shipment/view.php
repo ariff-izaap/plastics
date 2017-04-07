5372d5afe7818add60d953f0ca9d798c1a8d0a79
Punitha <punitha.lakshmi87@gmail.com>
2017-04-07 20:29:25 +0530

    sathiya shipmend page work and design fixes

--- application/views/frontend/shipment/view.php
+++ application/views/frontend/shipment/view.php
@@ -1,11 +1,9 @@
-<div class="container">
+<div class="container ma-top shipmend-sec">
 <div class="row">
   <div class="col-md-12">
   <form name="update_ship_status" method="POST" >
-    <div class="form-group col-md-4">
-        <p>Shipping Type <?php echo $ship_data['shipping_type']; ?></p>
-    </div>
-    <div class="form-group col-md-4"  >
+        <h3>Shipping Type <?php echo $ship_data['shipping_type']; ?></h3>
+    <div class="form-group ">
       <label>Carrier</label>
       <select name="carrier" class="form-group">
         <option value="">Select Carrier</option>
@@ -14,7 +12,7 @@
         <?php }} ?>
       </select>
     </div>
-    <div class="form-group col-md-4"  >
+    <div class="form-group ">
       <label>Status</label>
       <select name="status" class="form-group">
         <option value="">Select Status</option>
@@ -23,6 +21,25 @@
         <?php }} ?>
       </select>
     </div>
+     <div class="form-group ">
+      <label>Status</label>
+      <select name="status" class="form-group">
+        <option value="">Select Status</option>
+        <?php if(count($status)>0){ foreach($status as $ckey => $cvalue){ ?>
+         <option value="<?php echo $cvalue['id']; ?>" <?php echo set_select('status',$cvalue['id'],(($ship_data['order_status'] == $cvalue['id'])?true:false));?> ><?php echo $cvalue['name']; ?></option>
+        <?php }} ?>
+      </select>
+    </div>
+ <div class="form-group ">
+      <label>Status</label>
+      <select name="status" class="form-group">
+        <option value="">Select Status</option>
+        <?php if(count($status)>0){ foreach($status as $ckey => $cvalue){ ?>
+         <option value="<?php echo $cvalue['id']; ?>" <?php echo set_select('status',$cvalue['id'],(($ship_data['order_status'] == $cvalue['id'])?true:false));?> ><?php echo $cvalue['name']; ?></option>
+        <?php }} ?>
+      </select>
+    </div>
+
     <div class="form-group">
         <input type="submit" name="update_shp_type" class="btn btn-default" value="SAVE" />
     </div>
