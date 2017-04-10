dccb7bb514dd5fa41da03e67573503d8d5c06f10
Punitha <punitha.lakshmi87@gmail.com>
2017-04-10 18:04:39 +0530

    search page design fix

--- application/views/frontend/sales/search.php
+++ application/views/frontend/sales/search.php
@@ -26,56 +26,58 @@
         
         <!-- panel one-->
         
-        <div class="panel-heading formcontrol-box">
+        <div class="panel-heading formcontrol-box search-f1 search-f2 ">
         <div class="form-group" >
-          <label class="col-md-4">Contact Name</label>
-          <input type="text" name="contact_name" class="form-control col-md-8" id="contact_name" value=""  />
+          <label class="col-md-3 one-row-label">Contact Name</label>
+          <input type="text" name="contact_name" class="form-control one-row-input" id="contact_name" value=""  />
         </div>
-        <div class="form-group" >
-          <label class="col-md-4">Number</label>
-          <input type="text" name="contact_number" class="form-control col-md-8" id="contact_number" />
+        <div class="form-group col-md-6">
+          <label class="col-md-3">Number</label>
+          <input type="text" name="contact_number" class="form-control" id="contact_number" />
         </div>
-        <div class="form-group">
-          <label class="col-md-4">Contact Email</label>
-          <input type="text" name="contact_email" class="form-control col-md-8" id="contact_number" />
+        <div class="form-group col-md-6">
+          <label class="col-md-3">Contact Email</label>
+          <input type="text" name="contact_email" class="form-control" id="contact_number" />
         </div>
         </div>
         <!-- panel two-->
          <div class="panel-heading formcontrol-box second-box">
         <h2>Bill To</h2>
-        <div class="form-group">
-          <label class="col-md-4">Name</label>
-          <input type="text" name="bill_name" class="form-control col-md-8" id="bill_name" />
+        <div class="search-split-left">
+        <div class="form-group search-f3">
+          <label class="col-md-3">Name</label>
+          <input type="text" name="bill_name" class="form-control" id="bill_name" />
         </div>
-        <div class="form-group">
-          <label class="col-md-4">Address</label>
-          <input type="text" name="bill_address" class="form-control col-md-8" id="bill_address" />
+        <div class="form-group search-f3">
+          <label class="col-md-3">Address</label>
+          <input type="text" name="bill_address" class="form-control" id="bill_address" />
         </div>
-        <div class="form-group">
-          <label class="col-md-4">Address 2</label>
-          <input type="text" name="bill_address2" class="form-control col-md-8" id="bill_address2" />
+        <div class="form-group search-f3">
+          <label class="col-md-3">Address 2</label>
+          <input type="text" name="bill_address2" class="form-control" id="bill_address2" />
         </div>
-        <div class="form-group clearfix postal">
-        <div class="form-group col-md-2 city">
-          <label>City</label>
+        </div>
+        <div class="form-group clearfix postal search-split-right">
+        <div class="form-group city search-f4">
+          <label class="col-md-3">City</label>
           <input type="text" name="city" class="form-control" id="city" />
         </div>
         
-        <div class="form-group col-md-2">
-          <label>State</label>
+        <div class="form-group search-f4">
+          <label class="col-md-3">State</label>
           <input type="text" name="state" class="form-control" id="state" />
         </div>
-        <div class="form-group col-md-2 zip">
-          <label>Zip</label>
+        <div class="form-group zip search-f4">
+          <label class="col-md-3">Zip</label>
           <input type="text" name="zip" class="form-control" id="zip" />
         </div>
         </div>
-        <div class="form-group ">
-          <label class="col-md-4">Website</label>
-          <input type="text" name="website" class="form-control col-md-8" id="website" />
+        <div class="search-split-left">
+        <div class="form-group search-f3">
+          <label class="col-md-3">Website</label>
+          <input type="text" name="website" class="form-control" id="website" />
         </div>
-        <div class="row active-css">
-        <div class="col-md-6">
+        <div class="row active-css search-checkb">
         <div class="form-group">
           <input type="checkbox" name="active" class="" id="active" />
            <label>Active</label>
@@ -89,20 +91,21 @@
           <label>Customer</label>
         </div>
       </div>
-      <div class="col-md-6">
-        
-        <div class="form-group ">
-          <label>UPS</label>
+  
+        </div>
+            <!-- <div class="col-md-6"> -->
+        <div class="search-split-right pding-align">
+        <div class="form-group search-f4">
+          <label class="col-md-3">UPS</label>
           <input type="text" name="ups" class="form-control" id="ups" />
         </div>
         </div>
-        </div>
-        
+        <!-- </div> -->
         
         </div>
       </div>
       <div class="form-grid col-md-6 panel panel-default panel-bor">
-      <div class="panel-heading">
+      <div class="panel-heading third-box">
       <div class="call-group">
         <div class="form-group call">
           <label>Last Call Search</label>
@@ -113,14 +116,12 @@
           <input type="text" class="form-control"  name="time" id="time"  />
         </div>
         <div class="form-group time-choose">
-            <br />
             <select class="form-control">
             <option value="volvo"> < </option>
             <option value="saab"> > </option>
             </select> 
         </div>
         <div class="form-group use">
-        <br />
           <input type="checkbox"  name="customer_number" id="customer_number"  />
           <label>Use ?</label>
         </div>
