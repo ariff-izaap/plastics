dcdf7f78c2bc1669eaef7e651c8fb1217712c83d
Punitha <punitha.lakshmi87@gmail.com>
2017-04-22 16:21:07 +0530

    accunting section done - 22-04-2017

--- application/views/accounting/add_edit_invoices.php
+++ application/views/accounting/add_edit_invoices.php
@@ -37,95 +37,98 @@
   <input type="hidden" name="po_id" value="<?=$so_details[0]['po_id'];?>">
   <div class="container">
   <div class="row">
-    <div class="col-md-6">
+    <div class="form-grid col-md-6 panel panel-default panel-bor s-p-box1">
+    <div class="panel-heading formcontrol-box prod-sel account-invoice">
       <div class="form-group">
-        <label class="col-md-3"> Shipping Order(s) :</label>
+        <label class="col-md-6"> Shipping Order(s) :</label>
         <div class="col-md-6">
           <textarea name="so_id" class="form-control" readonly><?=str_replace(",","\n",implode(",",$so_id));?></textarea>
         </div>
       </div>
-      <div class="clearfix"></div><br>
+      
       <div class="form-group">
-        <label class="col-md-3"> Salesman :</label>
+        <label class="col-md-6"> Salesman :</label>
         <div class="col-md-6">
           <textarea class="form-control" name="salesman" readonly><?=str_replace(",","\n",implode(",",$salesman));?></textarea>
         </div>
       </div>
-      <div class="clearfix"></div><br>
+     
       <div class="form-group">
-        <label class="col-md-3"> Shipping Date :</label>
+        <label class="col-md-6"> Shipping Date :</label>
         <div class="col-md-6">
           <textarea class="form-control" name="ship_date" readonly><?=str_replace(",","\n",implode(",",$ship_date));?></textarea>
         </div>
       </div>
-      <div class="clearfix"></div><br>
+      
       <div class="form-group">
-        <label class="col-md-3" > Customer Name :</label>
+        <label class="col-md-6" > Customer Name :</label>
         <div class="col-md-6">
          <input type="text" class="form-control" readonly value="<?=$so_details[0]['cname'];?>">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+     <div class="clearfix"></div>
       <div class="form-group">
-        <label class="col-md-3" > Billing Address 1 :</label>
+        <label class="col-md-6" > Billing Address 1 :</label>
         <div class="col-md-6">
          <input type="text" class="form-control" readonly value="<?=$so_details[0]['address_1'];?>">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+     
       <div class="form-group">
-        <label class="col-md-3" > Billing Address 2 :</label>
+        <label class="col-md-6" > Billing Address 2 :</label>
         <div class="col-md-6">
          <input type="text" class="form-control" readonly value="<?=$so_details[0]['address_2'];?>">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+      
       <div class="form-group">
-        <label class="col-md-3" > City :</label>
+        <label class="col-md-6" > City :</label>
         <div class="col-md-6">
          <input type="text" class="form-control" readonly value="<?=$so_details[0]['city'];?>">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+      
       <div class="form-group">
-        <label class="col-md-3" > State :</label>
+        <label class="col-md-6" > State :</label>
         <div class="col-md-6">
          <input type="text" class="form-control" readonly value="<?=$so_details[0]['state_name'];?>">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+     
       <div class="form-group">
-        <label class="col-md-3" > Country :</label>
+        <label class="col-md-6" > Country :</label>
         <div class="col-md-6">
          <input type="text" class="form-control" readonly value="<?=$so_details[0]['country_name'];?>">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+      
       <div class="form-group">
-        <label class="col-md-3" > Zipcode :</label>
+        <label class="col-md-6" > Zipcode :</label>
         <div class="col-md-6">
          <input type="text" class="form-control" readonly value="<?=$so_details[0]['zipcode'];?>">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+     
     </div>
-    <div class="col-md-6">
+    </div>
+    <div class="form-grid col-md-6 panel panel-default panel-bor s-p-box1">
+    <div class="panel-heading formcontrol-box prod-sel account-invoice">
       <div class="form-group">
-        <label class="col-md-3" > Invoice Date :</label>
+        <label class="col-md-6" > Invoice Date :</label>
         <div class="col-md-6">
           <input type="text" class="form-control datetime" name="invoice_date">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+      
       <div class="form-group">
-        <label class="col-md-3" > Due Date :</label>
+        <label class="col-md-6" > Due Date :</label>
         <div class="col-md-6">
           <input type="text" class="form-control datetime" name="due_date">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+      
       <div class="form-group">
-        <label class="col-md-3" > Terms :</label>
+        <label class="col-md-6" > Terms :</label>
         <div class="col-md-6">
           <select class="form-control" name="credit_type">
             <?php
@@ -142,7 +145,7 @@
           </select>
         </div>
       </div>
-      <div class="clearfix"></div><br>
+    
       <!-- <div class="form-group">
         <label class="col-md-3" > PO# :</label>
         <div class="col-md-6">
@@ -156,42 +159,43 @@
           <input type="text" class="form-control" readonly=""  value="<?=$so_details[0]['so_id'];?>">
         </div>
       </div> -->
-      <div class="clearfix"></div><br>
+      
       <div class="form-group">
-        <label class="col-md-3" > Amount :</label>
+        <label class="col-md-6" > Amount :</label>
         <div class="col-md-6">
           <input type="text" class="form-control" name="amount" value="<?=displayData(array_sum($total_amount),'money');?>" readonly>
         </div>
       </div>
-      <div class="clearfix"></div><br>
+      <div class="clearfix"></div>
       <div class="form-group">
-        <label class="col-md-3" > Prepaid COD :</label>
+        <label class="col-md-6" > Prepaid COD :</label>
         <div class="col-md-6">
           <input type="text" class="form-control" name="cod_fee" readonly value="<?=displayData(array_sum($cod_fee),'money');?>">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+    
       <div class="form-group">
-        <label class="col-md-3" > Freight Amount :</label>
+        <label class="col-md-6" > Freight Amount :</label>
         <div class="col-md-6">
           <input type="text" class="form-control" name="freight" readonly value="<?=displayData(array_sum($freight),'money');?>">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+     
       <div class="form-group">
-        <label class="col-md-3" > Additional Amount:</label>
+        <label class="col-md-6" > Additional Amount:</label>
         <div class="col-md-6">
           <input type="text" class="form-control" name="add_amt" readonly value="<?=displayData('0','money');?>">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+    
       <div class="form-group">
-        <label class="col-md-3" > Total Amount :</label>
+        <label class="col-md-6" > Total Amount :</label>
         <div class="col-md-6">
           <input type="text" class="form-control" name="total_amount" readonly value="<?=displayData($amount,'money');?>">
         </div>
       </div>
-      <div class="clearfix"></div><br>
+     
+    </div>
     </div>
   </div>
   </div>
